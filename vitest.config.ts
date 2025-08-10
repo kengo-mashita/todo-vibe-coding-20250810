646d3837e57a9e78ec7871636090496484b883b8
kengo-mashita <56785259+kengo-mashita@users.noreply.github.com>
2025-08-11 02:10:52 +0900

    add tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,24 @@
+// vitest.config.ts
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    include: ['tests/unit/**/*.{test,spec}.{ts,tsx}'],
-    globals: true,
-    environment: 'node',
-    environmentMatchGlobs: [['**/*.test.tsx', 'jsdom']],
-    setupFiles: ['tests/setup/vitest.setup.ts'],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
     alias: {
-      '@': new URL('./src', import.meta.url).pathname,
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+    conditions: ['browser', 'module', 'import'],
+  },
+  test: {
+    include: ['tests/**/*.{test,spec}.{ts,tsx}'],
+    environment: 'node',
+    environmentMatchGlobs: [['**/*.{test,spec}.tsx', 'jsdom']],
+    setupFiles: ['tests/setup/vitest.setup.ts'],
+    globals: true,
+    css: true,
+    coverage: { provider: 'v8' },
   },
 })
