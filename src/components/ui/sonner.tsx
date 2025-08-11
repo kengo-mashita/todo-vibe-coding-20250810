3b65520d40638d73ea5377bbc1e41927c88491d6
kengo-mashita <56785259+kengo-mashita@users.noreply.github.com>
2025-08-12 00:08:12 +0900

    Toasterコンポーネントからスタイルプロパティを削除し、プロパティの整理を行いました。

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,13 +10,10 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps['theme']}
       className="toaster group"
-      style={
-        {
-          '--normal-bg': 'var(--popover)',
-          '--normal-text': 'var(--popover-foreground)',
-          '--normal-border': 'var(--border)',
-        } as React.CSSProperties
-      }
+      position="bottom-right"
+      visibleToasts={4}
+      expand={true}
+      richColors
       {...props}
     />
   )
